e164770061177eb93b868d452561cc71d9b1a9b8
Myles Rademaker <mylesprademaker@gmail.com>
2025-06-01 19:12:50 -0700

    Fixed some logic in boat floating, created test level with test assets for better game feel

--- scenes/boat.cs
+++ scenes/boat.cs
@@ -1,40 +1,40 @@
 using Godot;
 using System;
 
-public partial class boat : RigidBody3D { 
+public partial class Boat : RigidBody3D { 
 	[Export]
-	public float float_force { get; set; } = 1.4f;
+	public float FloatForce { get; set; } = 1.4f;
 	[Export]
-	public float water_drag { get; set; } = 0.05f;
+	public float WaterDrag { get; set; } = 0.05f;
 	[Export]
-	public float water_angular_drag { get; set; } = 0.1f;
+	public float WaterAngularDrag { get; set; } = 0.1f;
 	
-	private Node3D probe_container;
+	private Node3D ProbeContainer;
 
 	private const float GRAVITY = 9.8f;
 	//Need to get water height from water mesh rather than hard coding
-	private const float water_height = 14.0f;
-	private bool submerged = false;
+	private const float WaterHeight = 14.0f;
+	private bool Submerged = false;
 
 	public override void _Ready() {
-		probe_container = GetNode<Node3D>("ProbeContainer");
+		ProbeContainer = GetNode<Node3D>("ProbeContainer");
 	}
 
 	public override void _PhysicsProcess(double delta) {
-		submerged = false;
-		foreach (Marker3D probe in probe_container.GetChildren()) {
-			var depth = (water_height - probe.GlobalPosition.Y);
+		Submerged = false;
+		foreach (Marker3D probe in ProbeContainer.GetChildren()) {
+			var depth = (WaterHeight - probe.GlobalPosition.Y);
 			if (depth > 0) {
-				submerged = true;
-				ApplyForce(Vector3.Up * float_force * GRAVITY * depth, probe.GlobalPosition - GlobalPosition);
+				Submerged = true;
+				ApplyForce(Vector3.Up * FloatForce * GRAVITY * depth, probe.GlobalPosition - GlobalPosition);
 			}
 		}
 	}
 
 	public override void _IntegrateForces(PhysicsDirectBodyState3D state){
-		if (submerged) {
-			state.LinearVelocity *= 1 - water_drag;
-			state.AngularVelocity *= 1 - water_angular_drag;
+		if (Submerged) {
+			state.LinearVelocity *= 1 - WaterDrag;
+			state.AngularVelocity *= 1 - WaterAngularDrag;
 		}
 	}
 }
